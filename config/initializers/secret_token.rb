cd925b013ebaff6058d56d32c945753e28395351
Thibault Prat-Hagler <thibault.hagler@gmail.com>
2012-11-04 20:32:18 +0100

    fresh start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capsule::Application.config.secret_token = '1b1c61652706247fe38344fe857754ba4691152dcf7d1596ceae0d8e0d41be0f6622aedb246fe17aae81e5e18b0ce7a2c0cd61762659fbdbabb95ae151701e23'
