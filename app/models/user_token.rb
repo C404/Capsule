72f24fd058246e4d5cd04c16f9c2cd01d14d50ab
Thibault Prat-Hagler <thibault.hagler@gmail.com>
2012-11-13 00:55:03 +0100

    #6 no more uniqueness since we actualy want more mail to use and we dont care to have doublon accounts ...

--- app/models/user_token.rb
+++ app/models/user_token.rb
@@ -1,7 +1,4 @@
 class UserToken < ActiveRecord::Base
   belongs_to :user
   attr_accessible :da_token, :da_email, :fb_token, :fb_email, :fb_username, :tw_token, :tw_username
-
-  validates :da_email, :uniqueness => true
-  validates :fb_email, :uniqueness => true
 end
