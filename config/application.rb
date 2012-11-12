a3d57f4e5629bbdff99345b3ca4d33a6598d817e
Thibault Prat-Hagler <thibault.hagler@gmail.com>
2012-11-12 02:32:33 +0100

    added locales urls except home index but all locales are ready to use and copycopter on his way pretty soon #9

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,10 @@ module Capsule
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
+
+    # To restrict rails-i18n gem to load only theese locales
+    config.i18n.available_locales = [:en, :fr]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
--- config/routes.rb
+++ config/routes.rb
@@ -1,76 +1,25 @@
 Capsule::Application.routes.draw do
 
-  resources :capsuls
-
-  get "user/me"
   # auth and all sessions / connect controllers
-  devise_for :users, :path_names => { :sign_up => "register" }
   match '/auth/facebook/callback', to: 'omniauth#facebook'
   match '/auth/dailymotion/callback', to: 'omniauth#dailymotion'
   get "omni_session/connect"
+  
+#  match '/:locale' => 'home#index'
+  scope '(:locale)', constraints: { locale: /en|fr/ } do
 
-  # home route (index of website)
-  get "home/index"
+    # all capsuls CRUD
+    resources :capsuls
 
-  # any user profil exept current_user profil
-  resources :user, :only => [:show]
+    #devise auth and sessions user
+    devise_for :users, :path_names => { :sign_up => "register" }
 
+    # any user profil exept current_user profil
+    get "user/me"
+    resources :user, :only => [:show]
 
+    # root_path for /:locale
+    root :to => 'home#index'
+  end
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'home#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
