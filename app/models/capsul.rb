688300ca7f241a2e2fcccf8334d81f64416bbae9
Sylvain Cliquot <grochophone@gmail.com>
2012-11-16 03:57:23 +0100

    Ajout automatique des tags a la capsule cree #12

--- app/models/capsul.rb
+++ app/models/capsul.rb
@@ -3,4 +3,13 @@ class Capsul < ActiveRecord::Base
   has_and_belongs_to_many :tags
   acts_as_gmappable :process_geocoding => false
   attr_accessible :description, :video, :longitude, :latitude
+
+  after_create :append_tags
+
+  private
+  def append_tags
+    self.description.split.each do |word|
+      self.tags << Tag.find_or_create_by_tag(/^#(?<tag>\w+)/.match(word)[:tag]) if word =~ /^#(?<tag>\w+)/
+    end
+  end
 end
